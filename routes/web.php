785ca484044194a53f9121b2d2d8f7b50e0a3c4d
HarisPham <hakuriuhafini@gmail.com>
2025-03-11 21:47:36 +0700

    merge file from branch main

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SinhvienController;
 
 Route::get('/', [HomeController::class, 'trangchu'])->name('trangchu');
 Route::get('/sinhvien', [SinhvienController::class, 'index'])->name('sinhviens.index');
-=======
-
-Route::get('/', function () {
-    return view('welcome');
-});
->>>>>>> main
