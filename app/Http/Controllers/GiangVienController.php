46f828c9c3c1f28206a74fc31a5b22b9c1dd9ebc
viethung04 <hungvietnguyen2004@gmail.com>
2025-03-14 05:58:23 +0700

    update giao dien

--- app/Http/Controllers/GiangVienController.php
+++ app/Http/Controllers/GiangVienController.php
@@ -13,11 +13,38 @@ class GiangVienController extends Controller
         return view('giangviens.index', compact('giangviens'));
     }
 
-    public function destroy($id)
-    {
-        $giangvien = GiangVien::findOrFail($id);
-        $giangvien->delete();
 
-        return redirect()->route('giangviens.index')->with('success', 'Giảng viên đã được xóa thành công.');
+    public function edit($ma_gv)
+    {
+        $giangvien = giangvien::findOrFail($ma_gv);
+        return view('admin/giangvien.edit', compact('giangvien'));
+    }
+
+    public function update(Request $request, $ma_gv)
+    {
+        $request->validate([
+            'ma_gv' => 'required|string|max:20',
+            'ho_ten' => 'required|string|max:255',
+            'email' => 'required|email|max:100|unique:tai_khoan,email,' . $ma_gv,
+            'vai_tro' => 'required|in:Sinh viên,Giảng viên',
+            'sdt' => 'required|string|max:15',
+        ]);
+
+        $giangvien = giangvien::findOrFail($ma_gv);
+        $giangvien->update([
+            'ma_gv' => $request->ma_gv,
+            'ho_ten' => $request->ho_ten,
+            'email' => $request->email,
+            'vai_tro' => $request->vai_tro,
+            'sdt' => $request->sdt
+        ]);
+        return redirect()->route('admin/giangvien.index')->with('success', 'Sửa thành công!');
+    }
+
+    public function destroy($ma_gv)
+    {
+        $giangvien = giangvien::findOrFail($ma_gv);
+        $giangvien->delete();
+        return redirect()->route('admin/giangvien.index')->with('success', 'Xóa thành công!');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,11 @@ use App\Http\Controllers\GiangVienController;
 Route::get('/', [HomeController::class, 'trangchu'])->name('trangchu');
 Route::get('/sinhvien', [SinhvienController::class, 'index'])->name('sinhviens.index');
 Route::get('/giangvien', [GiangVienController::class, 'index'])->name('giangvien.index');
+
+
+
+// Các route khác
+Route::get('/giangvien/edit', [GiangVienController::class, 'edit'])->name('giangvien.edit');
+Route::put('/giangvien', [GiangVienController::class, 'update'])->name('giangvien.update');
+Route::delete('/giangvien', [GiangVienController::class, 'destroy'])->name('giangvien.destroy');
+Route::post('/giangvien', [GiangVienController::class, 'store'])->name('giangvien.store');
