bc0902840cc7062cdb2ff4336d3d24efa07b177a
Ph11 <phongphu7015@gmail.com>
2025-03-18 23:00:10 +0700

    fix database migration

---
+++ database/migrations/2025_03_09_053518_create_sinh_vien_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up() {
+        Schema::create('sinh_vien', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->unique()->primary();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('ho_ten', 100);
+            $table->date('ngay_sinh');
+            $table->enum('gioi_tinh', ['Nam', 'Nữ']);
+            $table->string('lop', 20);
+            $table->string('sdt', 15);
+            $table->string('email', 100)->unique();
+            $table->text('dia_chi');
+            $table->timestamps();
+        });
+    }
+
+    public function down() {
+        Schema::dropIfExists('sinh_vien');
+    }
+};
+
---
+++ database/migrations/2025_03_09_053519_create_ket_qua_thuc_tap_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up() {
+        Schema::create('ket_qua_thuc_tap', function (Blueprint $table) {
+            $table->string('ma_ket_qua', 20)->primary();
+            $table->unsignedBigInteger('ma_sv');
+            $table->unsignedBigInteger('ma_gv');
+            $table->string('ma_do_an', 20);
+            $table->float('diem_so')->nullable();
+            $table->text('nhan_xet_cua_giang_vien')->nullable();
+            $table->text('nhan_xet_cua_doanh_nghiep')->nullable();
+            $table->string('ten_dn');// ten doanh nghiep
+            $table->foreign('ma_sv')->references('user_id')->on('sinh_vien')->onDelete('cascade');
+            $table->foreign('ma_gv')->references('user_id')->on('giang_vien')->onDelete('cascade');
+            // $table->foreign('ma_do_an')->references('ma_do_an')->on('do_an')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down() {
+        Schema::dropIfExists('ket_qua_thuc_tap');
+    }
+};
+
